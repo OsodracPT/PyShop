d07011818142abe70bc025b981a6076e944391a4
Pedro Cardoso <pedroosodracwork@gmail.com>
2019-02-18 21:46:37 +0000

    Created the products app

---
+++ products/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# /products -> index
+def index(request):
+    return HttpResponse('Hello World')
+
+def new(request):
+    return HttpResponse('Create new product')
